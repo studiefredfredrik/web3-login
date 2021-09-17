7c721f812980f4eb7256f7df0e2f30ee6d08d0f2
studiefredfredrik <studiefredfredrik@gmail.com>
2021-09-18 00:43:33 +0200

    sign works

--- src/frontend/src/views/Frontpage.vue
+++ src/frontend/src/views/Frontpage.vue
@@ -4,45 +4,108 @@
       <video ref="video" class="top-video" autoplay loop muted playsinline src="top-video-reversed.mp4"></video>
     </div>
 
-    <div class="header">JWT + WEB3</div>
+    <div class="header">WEB3 + JWT</div>
     <div class="subsection">
       <div class="block">
-        <div>Log in with wallet, </div>
-        <div>A button / link</div>
+        <h1>Log in with wallet</h1>
+        <div>Once you connect a wallet we can see the address. This can sometimes be enough for some read-operations, like on <a href="https://pancakeswap.finance">defi</a></div>
+        <div class="click" @click="connect()">Connect</div>
+        <div v-if="address">Address: {{address}}</div>
       </div>
       <img class="arrow" src="arrow.png"/>
       <div class="block">
-        <div>Some text</div>
-        <div>A button / link</div>
+        <h1>Sign message with wallet</h1>
+        <div>But if the user want to keep some data secret with our service then we need to verify their identity</div>
+        <div>The wallet signs a message with it's private key. We then use ecrecover to get the address that signed the message</div>
+        <div>The signed message is sent to the server.</div>
+        <div class="click" @click="sign()">Sign</div>
+        <div v-if="signedMessage">Signed message: {{signedMessage}}</div>
       </div>
       <img class="arrow" src="arrow.png"/>
       <div class="block">
-        <div>Some text</div>
-        <div>A button / link</div>
+        <h1>Read signed message on server</h1>
+        <div>We then use ecrecover to get the address that signed the message</div>
+        <div>Once this is done we know that the person that signed the message is the owner of said address</div>
+        <div>You can now create a user account for this user, or fetch an existing user account from a database</div>
+        <div>Then encode a JWT with a user object, secured by a secret-key on the server</div>
+        <div class="click" @click="sendToServer()">Send to server</div>
+        <div v-if="recoveredAddress">{{recoveredAddress}}</div>
+        <div v-if="jwt">{{jwt}}</div>
       </div>
       <img class="arrow" src="arrow.png"/>
       <div class="block">
-        <div>Some text</div>
-        <div>A button / link</div>
+        <h1>The JWT</h1>
+        <div>The JWT is a cryptographically signed string that contains a JSON object</div>
+        <div>It's stored in the cookie, and sent on all requests</div>
+        <div>The signing of the JWT means that the holder of the secret key can verify it's integrity</div>
+        <div>Meaning we can read it on the server and use it to identify the user on every request, since we can know that no one has changed it</div>
+        <div>Meaning we can read it on the server and know that no one has changed it</div>
+        <div class="click" @click="decodeJwt()">Decode JWT</div>
+        <div v-if="jwtDecoded">{{jwtDecoded}}</div>
       </div>
       <img class="arrow" src="arrow.png"/>
+      <div class="block">
+        <h1>Sign out</h1>
+        <div>Time for the user to leave</div>
+        <div class="click" @click="signOut">Sign out</div>
+      </div>
+    </div>
+    <div class="attribution">
+      <a href="https://www.pexels.com/video/rainbow-light-reflection-on-a-soap-bubble-4431322/">Video by Miguel Á. Padriñán from Pexels</a>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import Web3 from "web3"
+import * as jwt from 'jsonwebtoken'
+
 export default {
   name: "Frontpage",
   data(){
     return {
-
+      address: '',
+      signedMessage: '',
+      recoveredAddress: '',
+      jwt: '',
+      jwtDecoded: '',
+      serverUserResponse: '',
+      nonce: ''
     }
   },
   mounted() {
-
+    this.$refs.video.playbackRate = 1;
+    this.nonce = `I want to log in to the WEB3+JWT site (${Math.random()})`
   },
   methods: {
-
+    async connect() {
+      if (!window.ethereum) {
+        alert('You need Metamask to use this site...')
+        return
+      }
+      let accountList = await window.ethereum.request({method: 'eth_requestAccounts'});
+      this.address = accountList[0]
+    },
+    async sign() {
+      let web3 = new Web3(window.ethereum);
+      this.signedMessage = await web3.eth.personal.sign(this.nonce, this.address, 'password') // Note that password is not needed when using Metamask, as it handles password management internally
+    },
+    async sendToServer() {
+      let res = await axios.post('/api/login', {
+        signedMessage: this.signedMessage,
+        nonce: this.nonce
+      })
+      this.recoveredAddress = res.data.recovredAddress
+      this.jwt = res.data.jwt
+    },
+    decodeJwt(){
+      this.jwtDecoded = jwt.decode(this.jwt)
+    },
+    signOut(){
+      document.cookie = ''
+      location.reload()
+    }
   }
 }
 </script>
@@ -63,7 +126,15 @@ body{
 </style>
 
 <style scoped>
+.click{
+  color: #af14ca;
+  cursor: pointer;
+}
+
 .container{
+  position: absolute;
+  height: 100%;
+  width: 100%;
   opacity: 1;
   transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(
       0deg) rotateY(
@@ -72,9 +143,8 @@ body{
       0deg,
       0deg);
   transform-style: preserve-3d;
-  /*background-color: #4b19af;*/
   background-image: linear-gradient(#4b19af, #3a0f8f);
-  height: 100%;
+
 }
 
 .top-video{
@@ -131,15 +201,28 @@ body{
   display: flex;
   flex-direction: column;
   justify-content: center;
+  margin-bottom: 50px;
 }
 
 .block{
- margin: auto;
+  width: 1000px;
+  margin: auto;
 }
 
 .arrow{
   transform: rotate(180deg);
   height: 100px;
   margin: 10px auto;
+}
+
+.attribution{
+  position: relative;
+  bottom: 0;
+  left: 10px;
+  font-size: 8px;
+}
+a{
+  color: white;
+  text-decoration: none;
 }
 </style>
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let cookieParser = require('cookie-parser');
+let ethSigUtil = require('eth-sig-util');
+let jwt = require('jsonwebtoken');
 
 let app = express();
 
@@ -10,9 +12,22 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/favorites', function (req, res) {
-    res.send('test')
+app.post('/api/login', async function (req, res) {
+    if(!req.body.signedMessage) return res.status(400).send('400')
+
+    let recoveredAddress = ethSigUtil.recoverPersonalSignature({
+        data: req.body.nonce,
+        sig: req.body.signedMessage
+    });
+
+    let privateKey = fs.readFileSync('private.key');
+    let jwt = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256'});
+
+    res.send({
+        recoveredAddress: recoveredAddress,
+        jwt: jwt
+    })
 })
 
-console.log('Servering on: http://localhost:8080')
+console.log('Servering on: http://localhost:8070')
 app.listen(8070)
